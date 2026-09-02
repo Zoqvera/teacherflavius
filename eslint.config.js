0f009629aea6e29142851de5007fb2d0c292f644
Zoqvera Desenvolvimento Web <handinhandengl@gmail.com>
2026-09-02 10:53:43 -0300

    chore: lint auth guard service

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ module.exports = [
       "area_do_estudante.js",
       "acessos_dos_alunos.js",
       "auth.js",
+      "auth_guard_service.js",
       "auth_infrastructure.js",
       "auth_session_service.js",
       "google_auth_ui.js",
