1ebdb2e4407fca5e8647cd8aa2f731bb1ff6efc7
Zoqvera Desenvolvimento Web <handinhandengl@gmail.com>
2026-09-02 13:07:15 -0300

    chore: lint Google-only access

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ module.exports = [
       "acessos_dos_alunos.js",
       "professor_home.js",
       "resource_waiter.js",
+      "google_only_access.js",
       "student_access_service.js",
       "student_access_renderer.js",
       "auth.js",
