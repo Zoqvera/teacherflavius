bf7af8f2d171a1491363f92edfda239edba21322
Zoqvera Desenvolvimento Web <handinhandengl@gmail.com>
2026-09-03 10:39:54 -0300

    refactor: extract static publish requirements

---
+++ scripts/static_publish_requirements.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+REQUIRED_PUBLIC_PATHS = (
+    "index.html",
+    "404.html",
+    "robots.txt",
+    "sitemap.xml",
+    "error_monitor.js",
+    "resource_waiter.js",
+    "supabase_client_service.js",
+    "auth_navigation_service.js",
+    "student_data_utils.js",
+    "student_enrollment_service.js",
+    "analytics.js",
+    "analytics_utils.js",
+    "analytics_acquisition.js",
+    "analytics_forms.js",
+    "analytics_payments.js",
+    "quero-conhecer/index.html",
+    "cadastro/index.html",
+    "meu-progresso/index.html",
+    "responsive_compat.css",
+    "_headers",
+)
