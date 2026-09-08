43e307eb5b6e5b599ac909dfedc3ddd42f7c11d2
Zoqvera Desenvolvimento Web <handinhandengl@gmail.com>
2026-09-08 19:40:47 -0300

    Stop restoring legacy sales page on clean route

--- clean_route_loader.js
+++ clean_route_loader.js
@@ -19,7 +19,6 @@
     "/quarta-feira-18h/": "/quarta-feira-18h.html",
     "/quarta-feira-20h/": "/quarta-feira-20h.html",
     "/quarta-feira-21h/": "/quarta-feira-21h.html",
-    "/quero-conhecer/": "/quero_conhecer.html",
     "/quinta-feira-09h/": "/quinta-feira-09h.html",
     "/quinta-feira-10h/": "/quinta-feira-10h.html",
     "/quinta-feira-12h/": "/quinta-feira-12h.html",
